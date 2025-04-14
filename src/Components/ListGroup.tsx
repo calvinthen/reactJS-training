e185871f2d3d03d4f3de1dbea8c8c956b3434659
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-04-14 16:59:01 +0700

    dynamic active css selected using useState

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,14 +1,23 @@
+import { MouseEvent, useState } from "react";
 
 function ListGroup()
 {
     let items = ["New York", "SA", "Tokyo", "London", "Jakarta"];
-    // items = [];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    const handleClick = (event:MouseEvent) => console.log(event.target); 
     return (
         <>
             <h1> List </h1>
            {items.length === 0 && <p>No item found</p>} 
             <ul className="list-group">
-                {items.map((item)=> ( <li key={item}> {item}</li>))}
+                {items.map((item, index)=> ( 
+                    <li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item' } 
+                    key={item} 
+                    onClick={() => {setSelectedIndex(index);}} > 
+                        {item}
+                    </li>
+                ))}
             </ul>
         </>
     );
